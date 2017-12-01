6797152bbd20867b9a91df1219b8ee8218ea98f0
Shannon Nichols <shannonsn19@gmail.com>
2017-12-01 18:00:51 +0200

    hiring of a plumber

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+var output = document.querySelector('.output').innerHTML;
+var outputs = Handlebars.compile(output);
+
+// gets all the plumbers in the database
+$.ajax({
+    type: "GET",
+    url: "https://innogy-api.herokuapp.com/api/plumber"
+}).then(function(result) {
+    console.log(result);
+    document.querySelector(".display").innerHTML = outputs({
+        plumber: result
+    })
+});
+
 // adds a new plumber to the database
-$('.submit').on('click', function() {
+$('.submit').on('click', () => {
     var plumber = {
         firstName: document.querySelector('#firstName').value,
         lastName: document.querySelector('#lastName').value,
@@ -21,20 +35,27 @@ $('.submit').on('click', function() {
     alert("A new Plumber has been added")
 })
 
+var plumberID = null;
 
-$(".schedule").on("click", ()=>{
-    //     var employee = {
-    //     day: employeeData.day,
-    //     slot: employeeData.slot,
-    //     jobDescription: document.querySelector("#jobDescription").value,
-    //     employerName: document.querySelector("#employerName").value,
-    //     employerContact: document.querySelector("#employerContact").value
-    // }
-    // $.ajax({
-    //     type: "POST",
-    //     url: "https://innogy-api.herokuapp.com/api/",
-    //     data: employee
-    // })
-alert("help")
+$(".display").on("click", (event)=>{
+    plumberID = event.target.value;
+})
 
+$(".schedule").on("click", (event) => {
+    var employee = {
+        day: document.querySelector(".day").value,
+        slot: document.querySelector(".slot").value,
+        jobDescription: document.querySelector("#jobDescription").value,
+        employerName: document.querySelector("#employerName").value,
+        employerContact: document.querySelector("#employerContact").value
+    }
+    $.ajax({
+        type: "POST",
+        url: "https://innogy-api.herokuapp.com/api/" + plumberID + "/booking",
+        data: employee,
+        success: function(forceGet) {
+            location.reload(forceGet);
+        },
+    })
+alert("your plubmer is on there way")
 })
